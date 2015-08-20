9c442fc8e90fcb50931eeef2f3231288bf1293dd
olehalvor <olli408@hotmail.com>
2015-08-20 17:04:01 +0200

    meeeer

--- javascript/playersAndTables.js
+++ javascript/playersAndTables.js
@@ -1,29 +1,132 @@
-console.log('Loading script');
-var players = [];
-var removedPlayers = [];
+var names = [];  //contains names of currently playing players
+var players=[];  //Contains names of all players (playing and removed);
+var removedPlayers=[]; //contains names of removed players;
+var rowIds=[];   
+var buyIn = 100;
+var totalPot;
+var numberOfPlayers = 0;
+var rowsInPointTable = 0;
 
 
-function addPlayer(){
-  var person = prompt("Enter the name of the Player", "PokerPlayer1");
-  players.push(person);
-  $('#playerCount').text( (players.length-removedPlayers.length) +"/"+ players.length);
-  console.log("add Player");
+
+//todo: Sorter navn
+//todo: Uppercase og lowercase
+
+function setBuyIn(buyIn){
+	//buyIn = 100;
+	totalPot = buyIn * names.length;
+	removeAllRows();
+	drawTable();
+	$('#buyIn').text(buyIn);
+	console.log("buy in varialbe: "+buyIn);
+}
+
+function addPlayer(){ //adds the input person to the array "names"
+	var person = prompt("Enter the name of the Player", "PokerPlayer1");
+	names.push(person);
+	players.push(person);
+	numberOfPlayers=numberOfPlayers+1;
+	$('#playerTable').append('<tr><td id="lab'+person+'">'+person+'</td><td id="place'+person+'"> </td></tr>');
+	totalPot = buyIn * names.length;
+	refresh();
+	$('#playerCount').text( (players.length-removedPlayers.length) +"/"+ players.length);
+	console.log(person +" added!");
+}
+
+function contains(value, array){
+	var b = false;
+	for (i in array){
+		if (array[i]==value) b = true;
+	}
+	return b;
 }
 
 function removePlayer(){
-  players.push(player);
+	var person = prompt("Enter the name of the Player", "PokerPlayer1");
+	removedPlayers.push(person);
+	$('#playerCount').text( (players.length-removedPlayers.length) +"/"+ players.length);
+	if (!contains(person,names)){
+		console.log("name not found");
+		return false;
+	}
+	console.log(person+" found in: "+names);
+	console.log("removePlayer: "+person);
+	document.getElementById('lab'+person).style.textDecoration = "line-through";
+	$('#place'+person).append(numberOfPlayers);
+	numberOfPlayers = numberOfPlayers -1 ;
+	var pIndex = names.indexOf(person);
+	console.log("pindex: "+pIndex);
+	delete names[pIndex];
+	console.log(names);
+	if (numberOfPlayers<rowsInPointTable){
+		console.log("numberOfPlayers < rowsInPointTable" + numberOfPlayers + " "+rowsInPointTable);
+		$('#p'+numberOfPlayers).append(person);
+	}
+	document.getElementById('nameInput').focus();
+
 }
 
-function addListeners(){
-	$('#addPlayer').click(addPlayer);
-	$('#removePlayer').click(removePlayer);
+function refresh(){ //refreshes the table
+	removeAllRows();
+	drawTable();
 }
 
-function init(){
-
+function removeAllRows(){ 	//removes table
+	console.log("removing rows");
+	rowsInPointTable = 0;
+	for (id in rowIds){
+		$('#row'+id).remove();
+	}
 }
+
+function drawTable(){	//generates table
+	var points = calculatePoints(names);
+	for (point in points){
+		var money = ((( points[point] * totalPot   ) /100 ));
+		addRow(point,money) ;
+	}
+	document.getElementById('totalPot').innerHTML="Total pot: " + totalPot;
+	//document.getElementById('numberOfPlayers').innerHTML ="Number of players: " + names.length;
+}
+
+function addRow(number, points){  //adds a row in the table
+	$('#pointTable').append('<tr id="row'+number+'"><td>'+number+'</td><td>'+points+'</td><td id="p'+number+'"></td></tr>');
+	rowIds.push("row"+number);
+	rowsInPointTable = rowsInPointTable +1;
+}
+
+
+
+
+
+function calculatePoints(names){ //input is array of names, output is a list of percentages
+	if (names.length==0){
+		return [100];
+	}
+	if (names.length>=1&&names.length<=8){
+		return [75,25];
+	}
+	else if(names.length>=9&&names.length<=12){
+		return [50,30,20];
+	}
+	else if(names.length>=13&&names.length<=17){
+		return [45,28,15,12];
+	}
+	else if(names.length>=18&&names.length<=26){
+		return [40,27,16,10,7];
+	}
+	else if(names.length>=27){
+		return [36,24,16,10,8,6];
+	}
+}
+
+
+
 
 $(document).ready(function(){ //wait until the html is loaded before starting script
-	addListeners();
+	$('#addPlayer').click(addPlayer);
+	$('#removePlayer').click(removePlayer);
+	document.getElementById('totalPot').innerHTML="Total pot: 0,-";
+	setBuyIn(100);
 });
 console.log("Script done");
--- javascript/pokerLogic.js
+++ javascript/pokerLogic.js
@@ -5,6 +5,11 @@ var stopTime = new Date(); //the time when you last stopped the clock
 var diffTime = 0;          //the difference in time between each time the clock function is run - each time this is added to "tid"
 var dispTime = 0;          //the time that will be shown
 var start = false;        //Contains whether the clock is running or not
+var sound = false;      //stores if you have clicked to enable sound checkbox
+var pause = false;      //stores if you have clicked to enable pause checkbox
+
+var chipvalues = [];
+var chipcolors = [];
 
 
 var myVar;
@@ -12,6 +17,19 @@ var temp = 60000;
 var remaining=0;
 
 
+function checkSound(){
+  if (sound==true){
+    sound=false;
+  }else {sound=true;}
+  console.log("Sound is now: "+sound);
+}
+
+function checkPause(){
+  if (pause==true){
+    pause=false;
+  }else {pause=true;}
+  console.log("Pause is now: "+pause);
+}
 
 function displayTheTime(time){   //converts from JS Date format(or milliseconds) to a more readeable one (HH:MM:SS)
 	var d = new Date(time);
@@ -29,14 +47,14 @@ function update(){						 //this function is run over and over with x ms delay - 
 function pad(n){return n<10 ? '0'+n : n}  //Makes 1 into 01.. used by the timer/clock
 
 
-function main(){						//main() is called when Start/stop is pushed
-  if (start == false){					// start == false indicates that the clock was not running
+function main(){					       	//main() is called when Start/stop is pushed
+  if (start == false){				  	// start == false indicates that the clock was not running
     startTime = Date.now();
     myVar = setInterval(update,500);		//starts looping the update() function with a given delay in ms.
-    start = true;						//the clock is running
+    start = true;					      	//the clock is running
     console.log("Clock is running");
   }
-  else{	  								//Else contains the code that's executed when the clock is paused
+  else{	  								        //Else contains the code that's executed when the clock is paused
     clearInterval(myVar);					//Stops the update() function
   	start = false;
     time = time + diffTime;					//Adds the current time on the clock to the time variable
@@ -52,10 +70,12 @@ function reset(){	   //runs if the reset button is clicked
 }
 
 
-
-
-window.addEventListener('load',function(){
-  document.getElementById('reset').addEventListener('click',reset);
-  document.getElementById('pauseStart').addEventListener('click',main);
+$(document).ready(function(){ //wait until the html is loaded before starting script
+  $('#reset').click(reset);
+  $('#pauseStart').click(main);
+  $('#soundBtn').click(checkSound);
+  $('#pauseBtn').click(checkPause);
+  $('#totalPot').text("Total pot: 0,-");
+  setBuyIn(100);
 });
 console.log('script done!');
